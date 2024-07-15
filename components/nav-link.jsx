2ed0c4cfd55a259dd9e8226e356a91da976b9ae6
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-07-15 16:52:56 +0800

    Extract usePathname logic into separate component

---
+++ components/nav-link.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function NavLink({ href, children }) {
+	const path = usePathname();
+	const isActive = path.startsWith(href);
+
+	return (
+		<Link href={href} className={isActive ? 'active' : undefined}>
+			{children}
+		</Link>
+	);
+}
