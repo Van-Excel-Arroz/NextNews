7a3386a3ff06c0dd2edb401bf506bc2fd9fb461f
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-07-15 15:24:46 +0800

    Added dynamic filtering for news by year and month

--- components/news-list.jsx
+++ components/news-list.jsx
@@ -6,7 +6,10 @@ export default function NewsList({ news }) {
 			{news.map(newsItem => (
 				<li key={newsItem.id}>
 					<Link href={`/news/${newsItem.slug}`}>
-						<img src={`images/news/${newsItem.image}`} alt={newsItem.title} />
+						<img
+							src="https://images.unsplash.com/photo-1438109519352-a52c41243c1a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8"
+							alt={newsItem.title}
+						/>
 						<span>{newsItem.title}</span>
 					</Link>
 				</li>
