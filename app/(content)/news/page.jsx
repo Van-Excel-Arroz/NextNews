1e02efd0d8f2c7f7e811073fbeae00a4ee0a2a84
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-07-17 16:36:23 +0800

    Fetch data directly from the database and display it in UI

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,11 +1,13 @@
-import { DUMMY_NEWS } from '@/dummy-news';
 import NewsList from '@/components/news-list';
+import { getAllNews } from '@/lib/news';
 
-export default function NewsPage(params) {
+export default async function NewsPage(params) {
+	const news = getAllNews();
+
 	return (
 		<>
 			<h1>News Page</h1>
-			<NewsList news={DUMMY_NEWS} />
+			<NewsList news={news} />
 		</>
 	);
 }
